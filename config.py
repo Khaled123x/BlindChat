a257a2f96484dd0658f69ca4d649a9d9908e753d
Khaled123x <143716155+Khaled123x@users.noreply.github.com>
2023-08-31 20:10:16 +0600

    config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os 
 
 TOKEN = os.environ.get("TOKEN", "6503370664:AAF7TrDU_IjnM4ft47IX9BbSAsyiGgCk7xg")
-OWNER = os.environ.get("OWNER", "Kamrul3406x")
+OWNER = os.environ.get("OWNER", "kamrul_3406x")
 GROUP = os.environ.get("GROUP", "KHCloudFileHangOut")
 CHANNEL = os.environ.get("CHANNEL", "KHCloudFile")
 BOT_NAME = os.environ.get("BOT_NAME", "Blind Chat")
